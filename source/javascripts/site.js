c7aebf70eab69e2337afc96585729c980d7f341c
Michael Nosal <mnosal@mitre.org>
2024-07-08 15:54:02 -0400

    Promote first page of playbook to be homepage, add scrollspy to playbook menu

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,5 @@
 // This is where it all goes :)
+var scrollSpy = new bootstrap.ScrollSpy(document.getElementById('content'), {
+  target: document.getElementById('playbook_menu'),
+  method: 'position'
+});
