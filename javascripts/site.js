62c154bc0e10f22dd1c1b69244ad25b02ca6ddd6
mrnosal <mrnosal@users.noreply.github.com>
2024-07-24 21:06:19 +0000

    deploy: 11ae6ad6356038d1f9c1bcec033ff9a5ae6446a2

--- javascripts/site.js
+++ javascripts/site.js
@@ -3,3 +3,29 @@ var scrollSpy = new bootstrap.ScrollSpy(document.getElementById('content'), {
   target: document.getElementById('playbook_menu'),
   method: 'position'
 });
+
+const backToTopButton = document.querySelector('[data-js-hook="back-to-top-button"]');
+const focusableElements = document.querySelectorAll('a');
+
+const scrollOptions = {
+  top: 0,
+  left: 0,
+  behavior: 'smooth'
+};
+
+const supportsNativeSmoothScroll = 'scrollBehavior' in document.documentElement.style;
+
+function moveToTop(event) {
+  event.preventDefault();
+
+  // Scroll to top.
+  supportsNativeSmoothScroll ? window.scrollTo(scrollOptions) : window.scrollTo(scrollOptions.left, scrollOptions.top);
+
+  // Focus the first focusable element.
+  focusableElements[0].focus({
+    preventScroll: true,
+  });
+  focusableElements[0].blur();
+  return false;
+}
+backToTopButton.addEventListener('click', moveToTop);
